15c2524f8615734a9699f124709d223181fa5738
bmwilly <brandon.williams@kineticsocial.com>
2015-08-17 11:44:31 -0400

    time

--- julia/efficient-operators/speed_compare.jl
+++ julia/efficient-operators/speed_compare.jl
@@ -7,7 +7,8 @@ include("hos_homg.jl")
 
 # orders = [9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
 # orders = [8];
-msizes = [2,3,4,5,6,7];
+# msizes = [2,3,4,5,6,7];
+msizes = [8,9,10,11,12]
 
 # mesh sizes
 # mtimes = Float64[]

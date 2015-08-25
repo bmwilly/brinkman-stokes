328aa6c4428c6fc88b2c994d64c4c9327eb232b5
bmwilly <brandon.williams@kineticsocial.com>
2015-08-25 16:51:16 -0400

    remove flowplot

--- julia-parallel/matrix-free-tensor/sss.jl
+++ julia-parallel/matrix-free-tensor/sss.jl
@@ -1,6 +1,6 @@
 using ParallelSparseMatMul
 include("stokes_flow/solve_stokes.jl")
-include("graphs/flowplot.jl")
+# include("graphs/flowplot.jl")
 include("helpers/helper_functions.jl")
 
 domain = int(input("Choose problem (1/lid-driven cavity, 2/brinkman): "))

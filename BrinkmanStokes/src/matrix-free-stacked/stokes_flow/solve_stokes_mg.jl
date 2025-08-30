42c5da2c5a68fad4148b8e2b1ec7bbc5d954c8b0
Brandon Williams <bwilliams9@pm.me>
2025-08-30 23:55:11 +0200

    Remove unnecessary return

--- BrinkmanStokes/src/matrix-free-stacked/stokes_flow/solve_stokes_mg.jl
+++ BrinkmanStokes/src/matrix-free-stacked/stokes_flow/solve_stokes_mg.jl
@@ -14,64 +14,70 @@ reload("solvers/m_st_mg.jl")
 ###SOLVE_STOKES solve stokes problem
 function solve_stokes_mg()
 
-    kparams = square_stokes()
+	kparams = square_stokes()
 
-    x = kparams["x"]; y = kparams["y"]
-    xy = kparams["xy"]; xyp = kparams["xyp"]; mv = kparams["mv"]; bound = kparams["bound"]
+	x = kparams["x"]
+	y = kparams["y"]
+	xy = kparams["xy"]
+	xyp = kparams["xyp"]
+	mv = kparams["mv"]
+	bound = kparams["bound"]
 
-    println("imposing (enclosed flow) boundary conditions ...")
+	println("imposing (enclosed flow) boundary conditions ...")
 
-    # get RHS vector
-    fst = fbc(kparams)
-    gst = gbc(kparams)
-    rhs = vec([fst; gst])
+	# get RHS vector
+	fst = fbc(kparams)
+	gst = gbc(kparams)
+	rhs = vec([fst; gst])
 
-    ## STOKES problem
+	## STOKES problem
 
-    # set parameters
-    # hard-coded for now
-    tol = 1.0e-6
-    maxit = 100
+	# set parameters
+	# hard-coded for now
+	tol = 1.0e-6
+	maxit = 100
 
-    # specify matrix-free galerkin
-    K = u -> kfunbc(u, kparams)
+	# specify matrix-free galerkin
+	K = u -> kfunbc(u, kparams)
 
-    # construct preconditioner
-    println("multigrid preconditioning...")
-    nu = length(xy[:, 1]); nv = 2nu; np = 3length(xyp[:, 1])
+	# construct preconditioner
+	println("multigrid preconditioning...")
+	nu = length(xy[:, 1])
+	nv = 2nu
+	np = 3length(xyp[:, 1])
 
-    # set up GMG preconditioner
-    (mgdata, smooth_data, sweeps, stype, npre, npost, nc) = mg_diff(kparams)
+	# set up GMG preconditioner
+	(mgdata, smooth_data, sweeps, stype, npre, npost, nc) = mg_diff(kparams)
 
-    mparams = {
-        "nv" => nv,
-        "np" => np,
-        "Q" => u -> qfun(u, kparams),
-        "Qdiag" => u -> qfun_diag(u, kparams),
-        "mgdata" => mgdata,
-        "smooth_data" => smooth_data,
-        "nc" => nc,
-        "npre" => npre,
-        "npost" => npost,
-        "sweeps" => sweeps,
-    }
+	mparams = Dict(
+		"nv" => nv,
+		"np" => np,
+		"Q" => u -> qfun(u, kparams),
+		"Qdiag" => u -> qfun_diag(u, kparams),
+		"mgdata" => mgdata,
+		"smooth_data" => smooth_data,
+		"nc" => nc,
+		"npre" => npre,
+		"npost" => npost,
+		"sweeps" => sweeps,
+	)
 
-    println("GMRES iteration...")
+	println("GMRES iteration...")
 
-    # zero initial guess
-    x0 = vec(zeros(size(rhs)))
+	# zero initial guess
+	x0 = vec(zeros(size(rhs)))
 
-    # MG preconditioner
-    M = u -> m_st_mg(u, mparams)
+	# MG preconditioner
+	M = u -> m_st_mg(u, mparams)
 
-    K = LinearOperator(nv + np, Float64, K)
-    M = LinearOperator(nv + np, Float64, M)
+	K = LinearOperator(nv + np, Float64, K)
+	M = LinearOperator(nv + np, Float64, M)
 
-    (xst, flag, err, iter, resvec) = gmres(
-        K, rhs, length(rhs) - 1;
-        tol = tol, maxIter = maxit, M = M, x = x0, out = 1
-    )
+	(xst, flag, err, iter, resvec) = gmres(
+		K, rhs, length(rhs) - 1;
+		tol = tol, maxIter = maxit, M = M, x = x0, out = 1,
+	)
 
-    return (xst, kparams)
+	return (xst, kparams)
 
 end
--- BrinkmanStokes/src/matrix-free-tensor/graphs/flowplot.jl
+++ BrinkmanStokes/src/matrix-free-tensor/graphs/flowplot.jl
@@ -3,49 +3,49 @@ include("../../output_utils.jl")
 
 function flowplot(sol, kparams, domain)
 
-    x = kparams["x"]
-    y = kparams["y"]
-    xy = kparams["xy"]
-    xyp = kparams["xyp"]
-    mv = kparams["mv"]
-    bound = kparams["bound"]
-    ae = kparams["ae"]
-    bxe = kparams["bxe"]
-    bye = kparams["bye"]
-    bbxe = kparams["bbxe"]
-    bbye = kparams["bbye"]
-    nvtx = length(xy[:, 1])
-    nu = 2nvtx
-    np = 3length(xyp[:, 1])
+	x = kparams["x"]
+	y = kparams["y"]
+	xy = kparams["xy"]
+	xyp = kparams["xyp"]
+	mv = kparams["mv"]
+	bound = kparams["bound"]
+	ae = kparams["ae"]
+	bxe = kparams["bxe"]
+	bye = kparams["bye"]
+	bbxe = kparams["bbxe"]
+	bbye = kparams["bbye"]
+	nvtx = length(xy[:, 1])
+	nu = 2nvtx
+	np = 3length(xyp[:, 1])
 
-    u = sol[1:nu]
-    p = sol[(nu + 1):end]
-    ux = reshape(u[1:nvtx], length(x), length(y))'
-    uy = reshape(u[(nvtx + 1):end], length(x), length(y))'
+	u = sol[1:nu]
+	p = sol[(nu+1):end]
+	ux = reshape(u[1:nvtx], length(x), length(y))'
+	uy = reshape(u[(nvtx+1):end], length(x), length(y))'
 
-    # Get msize from kparams
-    msize = kparams["msize"]
+	# Get msize from kparams
+	msize = kparams["msize"]
 
-    # Create streamlines plot using unified output system
-    streamlines_file = get_output_file("matrix-free-tensor", domain, msize, "streamlines.png"; subdir = "plots")
-    figure(figsize = (10, 8))
-    streamplot(x, y, ux, uy, density = 4, color = ux)
-    axis([-1, 1, -1, 1])
-    title("Streamlines (Domain=$domain, Size=$msize)")
-    colorbar()
-    savefig(streamlines_file, dpi = 150, bbox_inches = "tight")
-    println("Streamlines plot saved to: $(streamlines_file)")
+	# Create streamlines plot using unified output system
+	streamlines_file = get_output_file("matrix-free-tensor", domain, msize, "streamlines.png"; subdir = "plots")
+	figure(figsize = (10, 8))
+	streamplot(x, y, ux, uy, density = 4, color = ux)
+	axis([-1, 1, -1, 1])
+	title("Streamlines (Domain=$domain, Size=$msize)")
+	colorbar()
+	savefig(streamlines_file, dpi = 150, bbox_inches = "tight")
+	println("Streamlines plot saved to: $(streamlines_file)")
 
-    # Create velocity field plot using unified output system
-    velocity_file = get_output_file("matrix-free-tensor", domain, msize, "velocity_field.png"; subdir = "plots")
-    figure(figsize = (10, 8))
-    # pcolor(x, y, kp, cmap = "Greys");
-    quiver(x, y, ux, uy, ux, scale = 20)
-    axis([-1, 1, -1, 1])
-    title("Velocity Field (Domain=$domain, Size=$msize)")
-    savefig(velocity_file, dpi = 150, bbox_inches = "tight")
-    println("Velocity field plot saved to: $(velocity_file)")
+	# Create velocity field plot using unified output system
+	velocity_file = get_output_file("matrix-free-tensor", domain, msize, "velocity_field.png"; subdir = "plots")
+	figure(figsize = (10, 8))
+	# pcolor(x, y, kp, cmap = "Greys");
+	quiver(x, y, ux, uy, ux, scale = 20)
+	axis([-1, 1, -1, 1])
+	title("Velocity Field (Domain=$domain, Size=$msize)")
+	savefig(velocity_file, dpi = 150, bbox_inches = "tight")
+	println("Velocity field plot saved to: $(velocity_file)")
 
-    # Close figures to free memory
-    return close("all")
+	# Close figures to free memory
+	close("all")
 end
